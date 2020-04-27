748b456f18ccdd36469fa979c99f605230ddb448
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-04-27 06:28:19 +0200

    final netlify deploy

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ const MainLayout = ({ children }) => {
   return (
     <>
       <GlobalStyles/>
-      <Navigation/>
+      {typeof window !== "undefined" && <Navigation/>}
       {children}
       <Footer/>
     </>
